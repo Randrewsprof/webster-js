c881b1a5608c35f2b8469300b8e1ba587b5fd2c5
Riley Andrews <riley.andrews@pioneerbible.org>
2025-07-25 14:37:41 -0500

    Switch back

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
-const dev = process.env.NODE_ENV === 'development';
 
 export default {
   kit: {
     adapter: adapter(),
     paths: {
-      base: dev ? '' : '/webster-js' // Match your repo name
-    },
-    prerender: {
-      default: true
+      base: '/webster-js' // <-- Replace with your actual GitHub repo name
     }
   }
 };
